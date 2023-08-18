c3bc0301af1a162a95bd33e70e3ddbf8b5dece78
ANIK SAHA <46953050+aniksaha12@users.noreply.github.com>
2023-08-19 01:13:07 +0530

    navbar added

---
+++ client/src/components/Navbar/Navbar.jsx
@@ -0,0 +1,53 @@
+import React from 'react';
+import SearchIcon from '@mui/icons-material/Search';
+import PersonOutlineIcon from '@mui/icons-material/PersonOutline';
+import FavoriteBorderIcon from '@mui/icons-material/FavoriteBorder';
+import AddShoppingCartIcon from '@mui/icons-material/AddShoppingCart';
+import { Link } from 'react-router-dom';
+import "./Navbar.css";
+
+export default function Navbar() {
+  return (
+    <div className='navbar'>
+        <div className='wrapper'>
+        <div className='left'>
+            <div className='item'>
+                <Link className='link' to ="/products/1">Women</Link>
+            </div>
+            <div className='item'>
+                <Link className='link' to ="/products/2">Men</Link>
+            </div>
+            <div className='item'>
+                <Link className='link' to ="/products/3">Children</Link>
+            </div>
+        </div>
+        <div className='center'>
+            <Link className='link' to="/">LOGO</Link>
+        </div>
+        <div className='right'>
+        <div className='item'>
+            <Link className='link' to="/">Homepage</Link>
+        </div>
+        <div className='item'>
+            <Link className='link' to="/">About</Link>
+        </div>
+        <div className='item'>
+            <Link className='link' to="/">Contact</Link>
+        </div>
+        <div className='item'>
+            <Link className='link' to="/">Store</Link>
+        </div>
+        <div className='icons'>
+            <SearchIcon/>
+            <PersonOutlineIcon/>
+            <FavoriteBorderIcon/>
+            <div className='cartIcon'>
+                <AddShoppingCartIcon/>
+                <span>0</span>
+            </div>
+        </div>
+        </div>
+        </div>
+    </div>
+  )
+}
